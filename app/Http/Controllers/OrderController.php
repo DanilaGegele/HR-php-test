4210d441a706f09a5524c87ce3c247e6848fc5a7
Aslangery <Aslangery@gmail.com>
2018-05-03 18:55:50 +0300

    Mail send

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\OrderCompleted;
 use Illuminate\Http\Request;
+use Mail;
 use App\Order;
 use App\Partner;
 use App\Http\Requests\StoreOrder;
+
+define('STATE_NEW',0);
+define('STATE_CONFIRMED',10);
+define('STATE_COMPLETED',20);
 
 class OrderController extends Controller
 {
@@ -18,7 +24,7 @@ class OrderController extends Controller
     {
     	$order=Order::find($id);
     	$partners=Partner::all();
-		$states=['0'=>'Новый', '10'=>'Подтвержден', '20'=>'Завершен'];
+		$states=[STATE_NEW=>'Новый', STATE_CONFIRMED=>'Подтвержден', STATE_COMPLETED=>'Завершен'];
     	return view('order_edit', ['order'=>$order, 'partners'=>$partners, 'states'=>$states]);
     }
     public function store(StoreOrder $request)
@@ -26,10 +32,19 @@ class OrderController extends Controller
 	    $order=Order::find($request->get('id'));
 	    $order->client_email=$request->get('client_email');
 	    $order->partner_id=$request->get('partner_id');
+	    $oldState=$order->status;
 	    $order->status=$request->get('status');
 	    if($order->save())
 	    {
-		    return redirect()->action('OrderController@index');
+		    if ($oldState!==$order->status && $order->status==STATE_COMPLETED)
+		    {
+				Mail::to($order->partner->email)->send(new OrderCompleted($order));
+				foreach ($order->products as $product)
+				{
+					Mail::to($product->vendor->email)->send(new OrderCompleted($order));
+				}
+		    }
+	    	return redirect()->action('OrderController@index');
 	    }
 	    return redirect()->back()->withErrors('Ошибка сохранения');
     }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function vendor()
+    {
+    	return $this->belongsTo('App\Vendor');
+    }
 }
